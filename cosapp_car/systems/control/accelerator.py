576b66f5c4412b9d1c2644125e778aee99e57b4e
Luca Miguel <luca.miguel@usp.br>
2023-09-05 15:47:02 +0200

    pre-commit

--- cosapp_car/systems/control/accelerator.py
+++ cosapp_car/systems/control/accelerator.py
@@ -1,8 +1,9 @@
 from cosapp.base import System
 
+
 class Accelerator(System):
     """Model of a car's accelerator pedal.
-    
+
     Inputs
     ------
     delta: float,
@@ -17,11 +18,11 @@ class Accelerator(System):
 
     def setup(self):
 
-        self.add_inward("delta", 0., desc="Pedal angular displacement from rest position", unit='')
-        self.add_inward("delta_max", 1., desc="Maximum displacement", unit='')
+        self.add_inward("delta", 0.0, desc="Pedal angular displacement from rest position", unit="")
+        self.add_inward("delta_max", 1.0, desc="Maximum displacement", unit="")
 
-        self.add_outward("w_command", 0., desc="command signal", unit='')
+        self.add_outward("w_command", 0.0, desc="command signal", unit="")
 
     def compute(self):
 
-        self.w_command = self.delta/self.delta_max
+        self.w_command = self.delta / self.delta_max
