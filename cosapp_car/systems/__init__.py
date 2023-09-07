7ec4b9dcde38cbc234db4da77838755b2e27d251
Luca Miguel <63428345+luca7084@users.noreply.github.com>
2023-09-07 11:46:38 +0200

    Steering wheel (#4)
    * Added steering wheel and tests
    
    * pre-commit

--- cosapp_car/systems/__init__.py
+++ cosapp_car/systems/__init__.py
@@ -1,4 +1,4 @@
-from cosapp_car.systems.control import Accelerator, Brakes
+from cosapp_car.systems.control import Accelerator, Brakes, SteeringWheel
 from cosapp_car.systems.engine import Engine
 from cosapp_car.systems.physics import Dynamics
 from cosapp_car.systems.tank import Tank
@@ -6,4 +6,4 @@ from cosapp_car.systems.wheel import Wheels
 
 from cosapp_car.systems.car import Car  # isort: skip
 
-__all__ = ["Accelerator", "Brakes", "Car", "Dynamics", "Engine", "Tank", "Wheels"]
+__all__ = ["Accelerator", "Brakes", "Car", "Dynamics", "Engine", "SteeringWheel", "Tank", "Wheels"]
---
+++ cosapp_car/systems/control/steering_wheel.py
@@ -0,0 +1,28 @@
+from cosapp.base import System
+
+
+class SteeringWheel(System):
+    """Model of a car's steering wheel.
+
+    Inputs
+    ------
+    angle: float,
+        how much the wheel is turned
+
+    Outputs
+    ------
+    phi: float,
+        inclination output
+
+    """
+
+    def setup(self):
+
+        self.add_inward("angle", 0.0, desc="turning angle of the wheel", unit="")
+        self.add_inward("factor", 20.0, desc="Reduction factor")
+
+        self.add_outward("phi", 0.0, desc="Inclination command", unit="")
+
+    def compute(self):
+
+        self.phi = self.angle / self.factor
